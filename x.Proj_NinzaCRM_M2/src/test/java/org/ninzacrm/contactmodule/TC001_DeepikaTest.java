2d2e145b8e9c995b5524e890eb43bc80d9990b63
Deepika <deepika.2003@gmail.com>
2026-04-28 11:29:24 +0530

    DeepikaTest Commit

---
+++ x.Proj_NinzaCRM_M2/src/test/java/org/ninzacrm/contactmodule/TC001_DeepikaTest.java
@@ -0,0 +1,12 @@
+package org.ninzacrm.contactmodule;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TC001_DeepikaTest {
+	@Test
+	public void tc001_DeepikaTest() {
+		Reporter.log("DeepikaTest",true);		
+	}
+
+}
