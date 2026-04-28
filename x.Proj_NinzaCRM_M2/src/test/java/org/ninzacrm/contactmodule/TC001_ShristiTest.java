2632a62da69828541f208608d82551decde90ab2
Shekhar <Shekhar@LAPTOP-8KJH567A>
2026-04-28 12:01:24 +0530

    Extra Space Removed Done

--- x.Proj_NinzaCRM_M2/src/test/java/org/ninzacrm/contactmodule/TC001_ShristiTest.java
+++ x.Proj_NinzaCRM_M2/src/test/java/org/ninzacrm/contactmodule/TC001_ShristiTest.java
@@ -2,11 +2,8 @@ package org.ninzacrm.contactmodule;
 
 import org.testng.Reporter;
 import org.testng.annotations.Test;
-
-@Test
 public class TC001_ShristiTest{
-	
-	
+	@Test
 	public void TC001_ShristiTest() {
 	Reporter.log("TC001_ShristiTest",true);
 	}
