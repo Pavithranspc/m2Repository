5f3b228c55832e28ce9f761ccb97f47c4cac54e9
sravanivartha <sravani19.yvu@gmail.com>
2026-04-28 11:44:15 +0530

    poojitha commit 1

---
+++ x.Proj_NinzaCRM_M2/src/test/java/org/ninzacrm/campaignmodule/TC001_PoojithaTest.java
@@ -0,0 +1,17 @@
+package org.ninzacrm.campaignmodule;
+
+import java.io.IOException;
+
+import org.tvam.bussinessutility.BaseClass;
+
+public class TC001_PoojithaTest extends BaseClass {
+
+}
+
+
+
+
+
+
+
+
