16a66feb694f25bfdb721eabf7d5dc285242af8b
deepakkeshari-source <deepak.keshari@madverse.co>
2026-04-28 12:00:36 +0530

    Work Done

--- x.Proj_NinzaCRM_M2/src/test/java/org/ninzacrm/campaignmodule/TC001_AnjaliTest.java
+++ x.Proj_NinzaCRM_M2/src/test/java/org/ninzacrm/campaignmodule/TC001_AnjaliTest.java
@@ -1,13 +1,9 @@
 package org.ninzacrm.campaignmodule;
-
 import org.testng.Reporter;
 import org.testng.annotations.Test;
-
 public class TC001_AnjaliTest {
-
     @Test
     public void TC001_AnjaliTest() {
-
         Reporter.log("TC001_AnjaliTest", true);
     }
 }
