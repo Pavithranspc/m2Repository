ccf5ca49f2c5dec9306a7ba82a8d0fb893a045d2
Shekhar <Shekhar@LAPTOP-8KJH567A>
2026-04-28 11:29:24 +0530

    TC001_ShristiTest For Basic TestNG Logging Validation

---
+++ x.Proj_NinzaCRM_M2/src/test/java/org/ninzacrm/contactmodule/TC001_ShristiTest.java
@@ -0,0 +1,13 @@
+package org.ninzacrm.contactmodule;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+@Test
+public class TC001_ShristiTest{
+	
+	
+	public void TC001_ShristiTest() {
+	Reporter.log("TC001_ShristiTest",true);
+	}
+}
