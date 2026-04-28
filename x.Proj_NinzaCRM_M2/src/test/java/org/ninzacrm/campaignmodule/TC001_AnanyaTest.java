6ed6f7f92c5f745dde5be73eb4cc971547d200a6
LENOVO <LENOVO@DESKTOP-2JL316I>
2026-04-28 12:00:45 +0530

    Spaces got removed

--- x.Proj_NinzaCRM_M2/src/test/java/org/ninzacrm/campaignmodule/TC001_AnanyaTest.java
+++ x.Proj_NinzaCRM_M2/src/test/java/org/ninzacrm/campaignmodule/TC001_AnanyaTest.java
@@ -1,13 +1,9 @@
 package org.ninzacrm.campaignmodule;
-
 import org.testng.Reporter;
 import org.testng.annotations.Test;
-
 public class TC001_AnanyaTest {
  @Test
   public void tc001_AnanyaTest() {
 	  Reporter.log("GitHub Data");
-	 
-	
 }
 }
