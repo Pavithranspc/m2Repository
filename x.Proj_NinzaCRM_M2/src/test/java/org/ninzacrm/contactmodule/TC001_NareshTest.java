0959e00034ed5b7845a4ae545b1f987728ca5ffb
Naresh <Naresh@Naresh>
2026-04-28 12:00:20 +0530

    Change commit

--- x.Proj_NinzaCRM_M2/src/test/java/org/ninzacrm/contactmodule/TC001_NareshTest.java
+++ x.Proj_NinzaCRM_M2/src/test/java/org/ninzacrm/contactmodule/TC001_NareshTest.java
@@ -1,5 +1,12 @@
 package org.ninzacrm.contactmodule;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
 
 public class TC001_NareshTest {
-
+@Test
+	public void tc001_nareshTest(){
+	
+		Reporter.log("GitHub data");
+		
+	}
 }
